64d4ca51db5bc6c177699e1a7782dd188dc738fb
manoj2712 <manojmanoj123patidar@gmail.com>
2022-11-08 12:55:53 +0530

    backend

---
+++ trash_terminator/BackEnd/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+const post = require("./routes/posts");
+
+dotenv.config();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/posts", post);
+app.get("/", (req, res) => {
+  res.send("Hello to Trash-Terminator");
+});
+const PORT = process.env.PORT || 5000;
+const url = process.env.CONNECTION_URL;
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
