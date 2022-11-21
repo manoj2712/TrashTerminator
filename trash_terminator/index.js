7c6bdef6fba649016c3fee9531ae8932eae7e9f3
manoj2712 <manojmanoj123patidar@gmail.com>
2022-11-21 17:58:25 +0530

    confog changes for deployment

--- trash_terminator/index.js
+++ trash_terminator/index.js
@@ -4,23 +4,37 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
-const post = require("./routes/posts");
+const post = require("./BackEnd/routes/posts");
+const path = require("path");
 
 dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/posts", post);
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
   res.send("Hello to Trash-Terminator");
 });
+
+app.use("/posts", post);
+
+if (process.env.ENV === "production") {
+  app.use(express.static(path.join(__dirname, "FrontEnd/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "FrontEnd", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("Hello to Trash-Terminator");
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 const url = process.env.CONNECTION_URL;
 mongoose
   .connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }) 
+  })
   .then(() =>
     app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
   )
